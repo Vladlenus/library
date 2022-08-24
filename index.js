17561e88deac0dd7355b1cb72359c08d58cbecab
Vladlenus <110651671+Vladlenus@users.noreply.github.com>
2022-08-24 05:05:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 
-// function f1(){
-//     let a=+document.querySelector('.inp').value;
-//     b=a;
-//     console.log(b);
-//     return b;
+document.querySelector('.btn').onclick= f1
+document.querySelector('.btn').onclick= n1
+document.querySelector('.btn2').onclick= f2
+
+function f1(){
+    let a=document.querySelector('.inp').value;
+    c=a;
+    console.log(c);
+    document.body.style.margin = '20px';
+    return c;
     
-// }
+}
+function n1(){
+    let a=document.querySelector('.name').value;
+    b=a;
+    console.log(b);
+    return b;
+    
+}
 
-// f1()
-
-fetch ('https://63054b1b697408f7edc49ae4.mockapi.io/tasks', {
+function f2(){
+fetch ('http://63054b1b697408f7edc49ae4.mockapi.io/tasks',{
     method:'POST',
     headers:{
         'Content-type': 'application/json'
     },
     body: JSON.stringify({
-    name: 'sds'
+        text:f1(),
+        name: n1()
+   
 })})
 .then(res=>res.json())
 .then(data=>document.getElementById('root').innerHTML= JSON.stringify(data))
+
+.then(data =>console.log(data))
 .catch(error =>{console.log(error)})
-// fetch ("https://63054b1b697408f7edc49ae4.mockapi.io/tasks")
-// .then(res => res.json())
-// .then(data => document.getElementById('root').innerHTML= JSON.stringify(data))
 
-
+}
